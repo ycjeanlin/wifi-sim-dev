3859fd99477e9c52375a76c0a1e9438284f6fb3b
Jean <yzl0083@outlook.com>
2013-09-24 09:50:55 +0800

    updateADCL is finished

--- MobileDevice.h
+++ MobileDevice.h
@@ -4,6 +4,7 @@
 #define EPSILON2 0.2
 #define Credits 100
 #define NUM_OF_INTERESTS 10
+#define NUM_OF_USER_INTERESTS 5
 #define BUFFER_SIZE 200
 
 
@@ -24,7 +25,7 @@ class MobileDevice{
 		uint32_t AdPktContainer[BUFFER_SIZE][5]; 
 		Vector<Check> checkContainer;	
 	private:
-		uint32_t nodeInterests[5];
+		uint32_t nodeInterests[NUM_OF_USER_INTERESTS];
 		double pktValue[NUM_OF_INTERESTS][3]; //ad contact likelihood
 		double checkValue[BUFFER_SIZE][3]; //check reward contact likelihood
 		uint32_t credits;
@@ -36,14 +37,20 @@ void MobileDevice::updateADCL(uint32_t interests[], const double pktValue[][]){
 	double directContactP;
 	double inDirectContactP;
 
-
+	for(int i=0;i<NUM_OF_USER_INTERESTS;i++){
 		//Calculate the directly contact likelihood
 		DirectContactP = this.pktValue[interests[i]][0];
 		this.pktValue[interests[i]][0]=(1-EPSILON1)*directContactP+EPSILON1;
-
+	}
+		
+	for (int i = 0; i < NUM_OF_INTERESTS; i++){
 		//Calculate indirectly contact likelihood
-		inDirectContactP = this.pktValue[interests[i]][1];
-		this.pktValue[interests[i]][1] = (1-EPSILON2)*pktValue[interests[i]][0]+EPSILON2*inDirectContactP;
+		inDirectContactP = this.pktValue[i][1];
+		this.pktValue[i][1] = (1-EPSILON2)*pktValue[i][0]+EPSILON2*inDirectContactP;
 
+		//Update ADCL
+		this.pktValue[i][2] = 1 - (1-this.pktValue[i][0])*(1-this.pktValue[i][1]);
+	}
+		
 
 }
